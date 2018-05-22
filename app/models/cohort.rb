d1d5665171e7c8c5726a3d4953b7f1be1348f996
maikimo <mike.p.monahan@gmail.com>
2018-05-22 16:10:03 -0400

    stuff

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -2,7 +2,7 @@ class Cohort < ApplicationRecord
   belongs_to :course
   has_one :cohort_instructor
   has_one :instructor, through: :cohort_instructor
-  has_many :cohort_student
+  has_many :cohort_student, dependent: :destroy
   has_many :students, through: :cohort_student
   validates :name, uniqueness: true
   validates :course_id, numericality: { integer_only: true, greater_than: 0 }
