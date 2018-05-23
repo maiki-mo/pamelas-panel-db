b1fec2d404f16e571ab652ca22706160265670f4
maikimo <mike.p.monahan@gmail.com>
2018-05-23 14:50:34 -0400

    updating stuff

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,6 @@ class Course < ApplicationRecord
   has_many :students, through: :cohorts
   has_many :instructors
   has_many :instructors, through: :cohorts
-  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, alert: "only allows letters" }
   validates :hours, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 200 }
 end
