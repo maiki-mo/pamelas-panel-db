e20848ed5ccf73fb59f34a8f073f640728cd2c0d
maikimo <mike.p.monahan@gmail.com>
2018-05-23 23:23:36 -0400

    fixing small things

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+  def index
+    @cohorts = Cohort.all
+    @students = Student.all
+    @instructors = Instructor.all
+    @courses = Course.all
+  end
 end
